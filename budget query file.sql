6dd1abcf537bdc70288ea6054a1c1e5fe20ea195
talha atif <talhaatif573@gmail.com>
2025-04-17 00:02:11 +0500

    Challenges + dummy sql file

---
+++ budget query file.sql
@@ -0,0 +1,13 @@
+create database budget_tracker;
+drop database budget_tracker;
+
+use budget_tracker;
+truncate users;
+truncate accounts;
+truncate budgets;
+truncate transactions;
+truncate budgets;
+truncate budget_categories;
+truncate user_roles;
+select * from user_roles;
+select * from users;
